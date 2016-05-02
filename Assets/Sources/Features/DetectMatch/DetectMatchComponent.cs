c635d8d662bffcc159edad3f7ebc64d382b40ee6
August Wasilowski <August.Wasilowski@incontextsolutions.com>
2016-05-02 16:29:27 -0500

    Added placeholders for DetectMatch system.

---
+++ Assets/Sources/Features/DetectMatch/DetectMatchComponent.cs
@@ -0,0 +1,7 @@
+﻿using Entitas;
+
+public class DetectMatchComponent : IComponent
+{
+    public int x;
+    public int y;
+}
---
+++ Assets/Sources/Features/DetectMatch/DetectMatchSystem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Sources.Features.DetectMatch
+{
+    class DetectMatchSystem
+    {
+    }
+}
